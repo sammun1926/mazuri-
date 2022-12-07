afcfac1793cf9c8bc84905f9838f4002b5cdf9be
sammun1926 <munorusam10@gmail.com>
2022-12-07 15:59:51 +0300

    first commit

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Catagory;
+use App\Models\product;
+
 
 class AdminController extends Controller
 {
@@ -36,6 +38,68 @@ class AdminController extends Controller
         return redirect()->back()->with('message','Catagory deleted successsfully');
 
     }
+
+    public function view_product()
+    {
+        $catagory=catagory::all();
+        return view('admin.product' , compact('catagory'));
+    }
+
+    public function add_product(Request $request)
+    {
+        $product=new product;
+
+        $product->title=$request->title;
+
+        $product->description=$request->description;
+
+        $product->price=$request->price;
+
+        $product->quantity=$request->quantity;
+
+        $product->discount_price=$request->dis_price;
+
+        $product->catagory=$request->catagory;
+
+        $image=$request->image;
+
+        $imagename=time().'.'.$image->getClientOriginalExtension();
+
+        $request->image->move('product',$imagename);
+
+        $product->image=$imagename;
+
+        $product->save();
+
+        return redirect()->back()->with('message','Product Added Successfully');
+        
+
+    }
+
+    public function show_product()
+    {
+        $product=product::all();
+        return view('admin.show_product',compact('product'));
+    }
+
+    public function delete_product($id)
+    {
+        $product=product::find($id);
+
+        $product->delete();
+
+        return redirect()->back()->with('message','Product Deleted Successfully');
+
+    }
+    public function update_product($id)
+
+    {
+        $product=product::find($id);
+
+        return view('admin.update_product',compact('product'));
+    }
+
+    
 
 
 }
--- routes/web.php
+++ routes/web.php
@@ -35,3 +35,13 @@ route::get('/view_catagory',[AdminController::class,'view_catagory']);
 route::post('/add_catagory',[AdminController::class,'add_catagory']);
 
 route::get('/delete_catagory/{id}',[AdminController::class,'delete_catagory']);
+
+route::get('/view_product',[AdminController::class,'view_product']);
+
+route::post('/add_product',[AdminController::class,'add_product']);
+
+route::get('/show_product',[AdminController::class,'show_product']);
+
+route::get('/delete_product/{id}',[AdminController::class,'delete_product']);
+
+route::get('/update_product/{id}',[AdminController::class,'update_product']);
